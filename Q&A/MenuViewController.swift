604cc06fd851c2ca3e6d75760fde655f0d24097b
ZoeLi <zoeli@tstartel.com>
2020-05-22 23:42:33 +0800

    deleteImage

--- Q&A/MenuViewController.swift
+++ Q&A/MenuViewController.swift
@@ -35,19 +35,5 @@ class MenuViewController: UIViewController {
         QUESTION_QUANTITY = 100
     }
     
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
